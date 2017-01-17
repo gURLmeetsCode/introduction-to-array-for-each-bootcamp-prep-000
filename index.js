d3f65d75c59a58d3fcc4244e6b8519cd148b37d6
natasha kelly <kellynatashav@yahoo.com>
2017-01-17 01:53:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var changeCompletely = () =>{
-  .forEach(function(element, index, array){
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
 
-  });
+var animals = ["dog", "fish", "cat"]
 
+doToElementsInArray(animals, changeCompletely)
+
+console.log(animals)
+
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
 }
